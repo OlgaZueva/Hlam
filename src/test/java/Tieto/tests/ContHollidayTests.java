b3b1529e7159159952a9d6594c1632e40f9f25e3
OlgaZueva <ozzii14@gmail.com>
2017-04-04 23:08:11 +0300

    04042014_2300

---
+++ src/test/java/Tieto/tests/ContHollidayTests.java
@@ -0,0 +1,106 @@
+package Tieto.tests;
+
+import Tieto.helpers.Asserts;
+import Tieto.helpers.DBHelper;
+import Tieto.helpers.GetDataHelper;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Title;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+public class ContHollidayTests {
+    private Asserts asserts = new Asserts();
+    private GetDataHelper dh = new GetDataHelper();
+    private DBHelper db = new DBHelper();
+    private Properties properties = new Properties();
+
+    private Map<String, Object> mapForSource = new HashMap<String, Object>();
+    private Map<String, Object> mapForSA = new HashMap<String, Object>();
+
+
+    @Description("Сравнение данных записей таблиц CONTHOLLIDAY ")
+    @Title("Сравнение данных записей таблиц CONTHOLLIDAY в RTest и MSCRUS")
+    @Test
+    public void RTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+//Для маленьких таблиц должен быть выбран count(*). Менять параметр тут.
+        int countRowsInSource = Integer.parseInt(properties.getProperty("system.RownumPool"));
+        String countSelectedRows = dh.getRoonumPool("contholliday.SOURCE.CountRowForPool");
+        ArrayList arrayRows = dh.getArray(countRowsInSource);
+        String sqlfromSource = (properties.getProperty("contholliday.SOURCE.RowByRownumPart1") + countRowsInSource
+                + properties.getProperty("contholliday.SOURCE.RowByRownumPart2"));
+//Сравнение данных
+        Connection connectionToRTest = db.connToRTest();
+        Statement statmentForRTest = db.stFromConnection(connectionToRTest);
+        ResultSet rsCountRowFromRTest = db.rsFromDB(statmentForRTest, countSelectedRows);
+        while (rsCountRowFromRTest.next()) {
+            for (int i = 0; i < arrayRows.size(); i++) {
+                String sqlRowByRownum = sqlfromSource + arrayRows.get(i);
+                //System.out.println("SQL from Source:" + sqlRowByRownum);
+                ResultSet rsFromRTest = db.rsFromDB(statmentForRTest, sqlRowByRownum);
+                while (rsFromRTest.next()) {
+                    mapForSource = dh.getMapFromSource(rsFromRTest);
+// У таблицы собственные ключи. Если менять, то тут.
+                    String sqlFromSA = (properties.getProperty("contholliday.MSCRUS.RowByPKFromSA") + " SELSKAB = " + rsFromRTest.getString("SELSKAB")
+                            + " and DATO = '" + rsFromRTest.getString("DATO") + "' and AFDELING = '" + rsFromRTest.getString("AFDELING") +"'");
+                    mapForSA = dh.getMapFromSA(mapForSource.size(), sqlFromSA);
+                }
+                rsFromRTest.close();
+                asserts.matchMaps(mapForSource, mapForSA);
+            }
+        }
+        rsCountRowFromRTest.close();
+        statmentForRTest.close();
+        connectionToRTest.close();
+    }
+
+    @Description("Сравнение данных записей таблиц CONTHOLLIDAY ")
+    @Title("Сравнение данных записей таблиц CONTHOLLIDAY в ITest и UNITY")
+    @Test
+    public void ITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+//Для маленьких таблиц должен быть выбран count(*). Менять параметр тут.
+        int countRowsInSource = Integer.parseInt(properties.getProperty("system.RownumPool"));
+        String countSelectedRows = dh.getRoonumPool("contholliday.SOURCE.CountRowForPool");
+        ArrayList arrayRows = dh.getArray(countRowsInSource);
+        String sqlfromSource = (properties.getProperty("contholliday.SOURCE.RowByRownumPart1") + countRowsInSource
+                + properties.getProperty("contholliday.SOURCE.RowByRownumPart2"));
+//Сравнение данных
+        Connection connectionToITest = db.connToITest();
+        Statement statmentForITest = db.stFromConnection(connectionToITest);
+        ResultSet rsCountRowFromITest = db.rsFromDB(statmentForITest, countSelectedRows);
+        while (rsCountRowFromITest.next()) {
+            for (int i = 0; i < arrayRows.size(); i++) {
+                String sqlRowByRownum = sqlfromSource + arrayRows.get(i);
+                //System.out.println("SQL from Source:" + sqlRowByRownum);
+                ResultSet rsFromITest = db.rsFromDB(statmentForITest, sqlRowByRownum);
+                while (rsFromITest.next()) {
+                    mapForSource = dh.getMapFromSource(rsFromITest);
+// У таблицы собственные ключи. Если менять, то тут.
+                    String sqlFromSA = (properties.getProperty("contholliday.UNITY.RowByPKFromSA") + " SELSKAB = " + rsFromITest.getString("SELSKAB")
+                            + " and DATO = '" + rsFromITest.getString("DATO") + "' and AFDELING = '" + rsFromITest.getString("AFDELING") +"'");
+                    mapForSA = dh.getMapFromSA(mapForSource.size(), sqlFromSA);
+                }
+                rsFromITest.close();
+                asserts.matchMaps(mapForSource, mapForSA);
+            }
+
+        }
+        rsCountRowFromITest.close();
+        statmentForITest.close();
+        connectionToITest.close();
+    }
+
+    private void getPropertiesFile() throws IOException {
+        properties.load(new FileReader(new File(String.format("src/test/resources/sql.properties"))));
+    }
+}
+
--- src/test/java/Tieto/tests/CountsInTablesTests.java
+++ src/test/java/Tieto/tests/CountsInTablesTests.java
@@ -149,6 +149,22 @@ public class CountsInTablesTests {
         asserts.assertRowCount(counRowsInSource, counRowsInSA);
     }
 
+    @Test
+    public void BookFakRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("bookfak.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookfak.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookFakITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("bookfak.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookfak.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
     @Test (enabled = false)
     public void BookGodsRTestVsMSCRUS() throws SQLException, IOException {
         getPropertiesFile();
@@ -162,6 +178,342 @@ public class CountsInTablesTests {
         getPropertiesFile();
         int counRowsInSource = dh.getCountRowsInITest("bookgods.SOURCE.CountRow");
         int counRowsInSA = dh.getCountRowsInSA("bookgods.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookKorRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("bookkor.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookkor.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookKorITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("bookkor.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookkor.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookLinRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("booklin.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("booklin.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookLinITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("booklin.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("booklin.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookManifestsRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("bookmanifests.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookmanifests.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookManifestsITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("bookmanifests.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookmanifests.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookMftFileRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("bookmftfile.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookmftfile.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookMftFileITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("bookmftfile.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookmftfile.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookMftRemarksRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("bookmftremarks.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookmftremarks.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookMftRemarksITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("bookmftremarks.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookmftremarks.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("book.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("book.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("book.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("book.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookVesselRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("bookvessel.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookvessel.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void BookVesselITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("bookvessel.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("bookvessel.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ContHollidayRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("contholliday.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("contholliday.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ContHollidayITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("contholliday.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("contholliday.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ContRepRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("contrep.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("contrep.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ContRepITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("contrep.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("contrep.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ControlOfficeRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("controloffice.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("controloffice.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ControlOfficeITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("controloffice.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("controloffice.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ContTypeRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("conttype.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("conttype.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ContTypeITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("conttype.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("conttype.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void EdiKonvRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("edikonv.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("edikonv.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void EdiKonvITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("edikonv.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("edikonv.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ExpVesselRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("expvessel.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("expvessel.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void ExpVesselITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("expvessel.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("expvessel.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void FaktPostRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("faktpost.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("faktpost.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void FaktPostlITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("faktpost.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("faktpost.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void HenvisRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("henvis.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("henvis.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void HenvisITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("henvis.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("henvis.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void KundeRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("kunde.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("kunde.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void KundeITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("kunde.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("kunde.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void OrdreLinRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("ordrelin.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("ordrelin.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void OrdreLinITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("ordrelin.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("ordrelin.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void OrdreRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("ordre.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("ordre.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void OrdreITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("ordre.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("ordre.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void SagKursRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("sagkurs.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("sagkurs.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void SagKursITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("sagkurs.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("sagkurs.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void SagRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("sag.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("sag.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void SagITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("sag.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("sag.UNITY.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void SelskabRTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInRTest("selskab.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("selskab.MSCRUS.CountRows");
+        asserts.assertRowCount(counRowsInSource, counRowsInSA);
+    }
+
+    @Test
+    public void SelsksbITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+        int counRowsInSource = dh.getCountRowsInITest("selskab.SOURCE.CountRow");
+        int counRowsInSA = dh.getCountRowsInSA("selskab.UNITY.CountRows");
         asserts.assertRowCount(counRowsInSource, counRowsInSA);
     }
 
