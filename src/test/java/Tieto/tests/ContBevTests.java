c971300e79bfab0b918dbf7f10643b72a6d8856d
OlgaZueva <ozzii14@gmail.com>
2017-08-29 12:51:57 +0300

    20170829_1251

---
+++ src/test/java/Tieto/tests/ContBevTests.java
@@ -0,0 +1,107 @@
+package Tieto.tests;
+
+import Tieto.helpers.Asserts;
+import Tieto.helpers.DBHelper;
+import Tieto.helpers.GetDataHelper;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Title;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+public class ContBevTests {
+    private Asserts asserts = new Asserts();
+    private GetDataHelper dh = new GetDataHelper();
+    private DBHelper db = new DBHelper();
+    private Properties properties = new Properties();
+
+    private Map<String, Object> mapForSource = new HashMap<String, Object>();
+    private Map<String, Object> mapForSA = new HashMap<String, Object>();
+
+
+    @Description("Сравнение данных записей таблиц CONTBEV ")
+    @Title("Сравнение данных записей таблиц CONTBEV в RTest и MSCRUS")
+    @Test
+    public void RTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+//Для маленьких таблиц должен быть выбран count(*). Менять параметр тут.
+        int countRowsInSource = Integer.parseInt(properties.getProperty("system.RownumPool"));
+        String countSelectedRows = dh.getRoonumPool("contBev.SOURCE.CountRowForPool");
+        ArrayList arrayRows = dh.getArray(countRowsInSource);
+        String sqlfromSource = (properties.getProperty("contBev.SOURCE.RowByRownumPart1") + countRowsInSource
+                + properties.getProperty("contBev.SOURCE.RowByRownumPart2"));
+//Сравнение данных
+        Connection connectionToRTest = db.connToRTest();
+        Statement statmentForRTest = db.stFromConnection(connectionToRTest);
+        ResultSet rsCountRowFromRTest = db.rsFromDB(statmentForRTest, countSelectedRows);
+        while (rsCountRowFromRTest.next()) {
+            for (int i = 0; i < arrayRows.size(); i++) {
+                String sqlRowByRownum = sqlfromSource + arrayRows.get(i);
+                //System.out.println("SQL from Source:" + sqlRowByRownum);
+                ResultSet rsFromRTest = db.rsFromDB(statmentForRTest, sqlRowByRownum);
+                while (rsFromRTest.next()) {
+                    mapForSource = dh.getMapFromSource(rsFromRTest);
+// У таблицы собственные ключи. Если менять, то тут.
+                    String sqlFromSA = (properties.getProperty("contBev.MSCRUS.RowByPKFromSA") + " SELSKAB = " + rsFromRTest.getString("SELSKAB")
+                            + " and BEV_TYPE = '" + rsFromRTest.getString("BEV_TYPE") + "' and KODE = '" + rsFromRTest.getString("KODE")
+                            + "' and LINIE = '" + rsFromRTest.getString("LINIE") + "'");
+                    mapForSA = dh.getMapFromSA(mapForSource.size(), sqlFromSA);
+                }
+                rsFromRTest.close();
+                asserts.matchMaps(mapForSource, mapForSA);
+            }
+        }
+        rsCountRowFromRTest.close();
+        statmentForRTest.close();
+        connectionToRTest.close();
+    }
+
+    @Description("Сравнение данных записей таблиц CONTBEV ")
+    @Title("Сравнение данных записей таблиц CONTBEV в RTest и UNITY")
+    @Test
+    public void ITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+//Для маленьких таблиц должен быть выбран count(*). Менять параметр тут.
+        int countRowsInSource = Integer.parseInt(properties.getProperty("system.RownumPool"));
+        String countSelectedRows = dh.getRoonumPool("contBev.SOURCE.CountRowForPool");
+        ArrayList arrayRows = dh.getArray(countRowsInSource);
+        String sqlfromSource = (properties.getProperty("contBev.SOURCE.RowByRownumPart1") + countRowsInSource
+                + properties.getProperty("contBev.SOURCE.RowByRownumPart2"));
+//Сравнение данных
+        Connection connectionToITest = db.connToITest();
+        Statement statmentForITest = db.stFromConnection(connectionToITest);
+        ResultSet rsCountRowFromITest = db.rsFromDB(statmentForITest, countSelectedRows);
+        while (rsCountRowFromITest.next()) {
+            for (int i = 0; i < arrayRows.size(); i++) {
+                String sqlRowByRownum = sqlfromSource + arrayRows.get(i);
+                //System.out.println("SQL from Source:" + sqlRowByRownum);
+                ResultSet rsFromITest = db.rsFromDB(statmentForITest, sqlRowByRownum);
+                while (rsFromITest.next()) {
+                    mapForSource = dh.getMapFromSource(rsFromITest);
+// У таблицы собственные ключи. Если менять, то тут.
+                    String sqlFromSA = (properties.getProperty("contBev.UNITY.RowByPKFromSA") + " SELSKAB = " + rsFromITest.getString("SELSKAB")
+                            + " and BEV_TYPE = '" + rsFromITest.getString("BEV_TYPE") + "' and KODE = '" + rsFromITest.getString("KODE")
+                            + "' and LINIE = '" + rsFromITest.getString("LINIE") + "'");
+                    mapForSA = dh.getMapFromSA(mapForSource.size(), sqlFromSA);
+                }
+                rsFromITest.close();
+                asserts.matchMaps(mapForSource, mapForSA);
+            }
+        }
+        rsCountRowFromITest.close();
+        statmentForITest.close();
+        connectionToITest.close();
+    }
+
+    private void getPropertiesFile() throws IOException {
+        properties.load(new FileReader(new File(String.format("src/test/resources/sql.properties"))));
+    }
+}
+
---
+++ src/test/java/Tieto/tests/DemurrageTests.java
@@ -0,0 +1,109 @@
+package Tieto.tests;
+
+import Tieto.helpers.Asserts;
+import Tieto.helpers.DBHelper;
+import Tieto.helpers.GetDataHelper;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Title;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+public class DemurrageTests {
+    private Asserts asserts = new Asserts();
+    private GetDataHelper dh = new GetDataHelper();
+    private DBHelper db = new DBHelper();
+    private Properties properties = new Properties();
+
+    private Map<String, Object> mapForSource = new HashMap<String, Object>();
+    private Map<String, Object> mapForSA = new HashMap<String, Object>();
+
+
+    @Description("Сравнение данных записей таблиц DEMURRAGE ")
+    @Title("Сравнение данных записей таблиц DEMURRAGE в RTest и MSCRUS")
+    @Test
+    public void RTestVsMSCRUS() throws SQLException, IOException {
+        getPropertiesFile();
+//Для маленьких таблиц должен быть выбран count(*). Менять параметр тут.
+        int countRowsInSource = Integer.parseInt(properties.getProperty("system.RownumPool"));
+        String countSelectedRows = dh.getRoonumPool("demurrage.SOURCE.CountRowForPool");
+        ArrayList arrayRows = dh.getArray(countRowsInSource);
+        String sqlfromSource = (properties.getProperty("demurrage.SOURCE.RowByRownumPart1") + countRowsInSource
+                + properties.getProperty("demurrage.SOURCE.RowByRownumPart2"));
+//Сравнение данных
+        Connection connectionToRTest = db.connToRTest();
+        Statement statmentForRTest = db.stFromConnection(connectionToRTest);
+        ResultSet rsCountRowFromRTest = db.rsFromDB(statmentForRTest, countSelectedRows);
+        while (rsCountRowFromRTest.next()) {
+            for (int i = 0; i < arrayRows.size(); i++) {
+                String sqlRowByRownum = sqlfromSource + arrayRows.get(i);
+                //System.out.println("SQL from Source:" + sqlRowByRownum);
+                ResultSet rsFromRTest = db.rsFromDB(statmentForRTest, sqlRowByRownum);
+                while (rsFromRTest.next()) {
+                    mapForSource = dh.getMapFromSource(rsFromRTest);
+// У таблицы собственные ключи. Если менять, то тут.
+                    String sqlFromSA = (properties.getProperty("demurrage.MSCRUS.RowByPKFromSA") + " BOOK_NO = " + rsFromRTest.getString("BOOK_NO")
+                            + " and CODE = " + rsFromRTest.getString("CODE") + " and COMPANY = " + rsFromRTest.getString("COMPANY")
+                            + " and CONT_NO = '" + rsFromRTest.getString("CONT_NO") + "' and DEM_ID = '" + rsFromRTest.getString("DEM_ID")
+                            + "' and STATUS = '" + rsFromRTest.getString("STATUS") + "'");
+                    mapForSA = dh.getMapFromSA(mapForSource.size(), sqlFromSA);
+                }
+                rsFromRTest.close();
+                asserts.matchMaps(mapForSource, mapForSA);
+            }
+        }
+        rsCountRowFromRTest.close();
+        statmentForRTest.close();
+        connectionToRTest.close();
+    }
+
+    @Description("Сравнение данных записей таблиц DEMURRAGE ")
+    @Title("Сравнение данных записей таблиц DEMURRAGE в RTest и UNITY")
+    @Test
+    public void ITestVsUNITY() throws SQLException, IOException {
+        getPropertiesFile();
+//Для маленьких таблиц должен быть выбран count(*). Менять параметр тут.
+        int countRowsInSource = Integer.parseInt(properties.getProperty("system.RownumPool"));
+        String countSelectedRows = dh.getRoonumPool("demurrage.SOURCE.CountRowForPool");
+        ArrayList arrayRows = dh.getArray(countRowsInSource);
+        String sqlfromSource = (properties.getProperty("demurrage.SOURCE.RowByRownumPart1") + countRowsInSource
+                + properties.getProperty("demurrage.SOURCE.RowByRownumPart2"));
+//Сравнение данных
+        Connection connectionToITest = db.connToITest();
+        Statement statmentForITest = db.stFromConnection(connectionToITest);
+        ResultSet rsCountRowFromITest = db.rsFromDB(statmentForITest, countSelectedRows);
+        while (rsCountRowFromITest.next()) {
+            for (int i = 0; i < arrayRows.size(); i++) {
+                String sqlRowByRownum = sqlfromSource + arrayRows.get(i);
+                //System.out.println("SQL from Source:" + sqlRowByRownum);
+                ResultSet rsFromITest = db.rsFromDB(statmentForITest, sqlRowByRownum);
+                while (rsFromITest.next()) {
+                    mapForSource = dh.getMapFromSource(rsFromITest);
+// У таблицы собственные ключи. Если менять, то тут.
+                    String sqlFromSA = (properties.getProperty("demurrage.UNITY.RowByPKFromSA") + " BOOK_NO = " + rsFromITest.getString("BOOK_NO")
+                            + " and CODE = " + rsFromITest.getString("CODE") + " and COMPANY = " + rsFromITest.getString("COMPANY")
+                            + " and CONT_NO = '" + rsFromITest.getString("CONT_NO") + "' and DEM_ID = '" + rsFromITest.getString("DEM_ID")
+                            + "' and STATUS = '" + rsFromITest.getString("STATUS") + "'");
+                    mapForSA = dh.getMapFromSA(mapForSource.size(), sqlFromSA);
+                }
+                rsFromITest.close();
+                asserts.matchMaps(mapForSource, mapForSA);
+            }
+        }
+        rsCountRowFromITest.close();
+        statmentForITest.close();
+        connectionToITest.close();
+    }
+
+    private void getPropertiesFile() throws IOException {
+        properties.load(new FileReader(new File(String.format("src/test/resources/sql.properties"))));
+    }
+}
+
